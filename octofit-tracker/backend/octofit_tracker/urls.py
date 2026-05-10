0fce0e5dcc5c2f783f605a6a61ee456af259457e
uno-dos-tres <nikita_tepliakov@epam.com>
2026-05-10 20:36:32 +0000

    Implement initial backend structure with models, serializers, views, admin, and tests for octofit-tracker app

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import api_root
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', api_root, name='api-root'),
+    path('', include('octofit_tracker.urls')),
 ]
